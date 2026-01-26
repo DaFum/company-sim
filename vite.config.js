af2ff500d444ff49f7a494c1572b820a4fb70e88
DaFum <150466734+DaFum@users.noreply.github.com>
2026-01-26 11:43:17 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,7 +11,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(projectRoot, 'src'),
+      '@': resolve(__dirname, 'src'),
     },
   },
 });
