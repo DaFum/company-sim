aa0fc804bc0d8429afbe7473e424d292c0ff7190
DaFum <150466734+DaFum@users.noreply.github.com>
2026-01-26 11:41:29 +0100

    Add alias resolution for src directory

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: process.env.GITHUB_PAGES === 'true' ? '/github-repository-vi/' : '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(projectRoot, 'src'),
+    },
+  },
 });
